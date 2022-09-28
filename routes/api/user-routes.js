be090997e9340e478f30a36b63e5f4cb90a59bc3
Micah <micahculverhouse1@gmail.com>
2022-09-28 00:56:38 -0600

    working on the routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    newFriend,
+    deleteFriend,
+} = require('../../controllers/user-controllers');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(newFriend).delete(deleteFriend);
+
+module.exports = router;
